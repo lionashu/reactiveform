483de9400e2ff0c2c1982af19cb779437e1be4cc
lionashu <ashishmipra@gmail.com>
2020-02-22 16:56:02 +0530

    Reactive Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'reactive-form';
+export class AppComponent implements OnInit {
+  title = 'Angular 9 Reactive-form and Validation';
+  regForm:FormGroup;
+  submitted = false;
+  public newData:any;
+
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(){
+    this.regForm = this.formBuilder.group({
+      firstName:['', Validators.required],
+      lastName:['', Validators.required],
+      email:['', [Validators.required, Validators.email]],
+      password:['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+  get formData(){ return this.regForm.controls;}
+
+  onSubmit(){
+    this.submitted = true;
+
+    if(this.regForm.invalid){
+      return;
+    }
+  alert(JSON.stringify(this.regForm.value));
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
